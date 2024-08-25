ae8ae9db28d32e92719bc7d022a4513d0f30191d
Sanjay Khuntia <isanjayk5@gmail.com>
2024-08-25 21:54:37 +0530

    two pointers examples

---
+++ Two Pointers/three_sum_values.py
@@ -0,0 +1,33 @@
+def find_threesum_values(nums, target):
+    nums.sort() # time complexity= O(nlog n), space complexity O(n)
+
+    # time complexity for nested loop= O(n^2)
+    for i in range(0, len(nums) - 2):
+        low = i + 1
+        high = len(nums) - 1
+
+        while low < high:
+            sum = nums[i] + nums[low] + nums[high]
+
+            if sum == target:
+                return True
+            elif sum < target:
+                low += 1
+            else:
+                high -= 1
+
+    return False
+
+
+def main():
+    num_list = [4, 6, 2, -5, 10, 9]
+    target_sum = 25
+
+    if find_threesum_values(num_list, target_sum):
+        print(f"Sum for {target_sum} exists in the list {num_list}")
+    else:
+        print(f"Sum for {target_sum} done not exist in the list {num_list}")
+
+
+if __name__ == '__main__':
+    main()
--- Two Pointers/valid_palindrome.py
+++ Two Pointers/valid_palindrome.py
@@ -3,16 +3,19 @@ def is_palindrome(s):
     right = len(s) - 1
     # use two pointers to traverse the string
     while left <= right:
-        if s[left] == s[right]:
-            left = left + 1
-            right = right - 1
-            continue
-        else:
+        if s[left] != s[right]:
             return False
 
+        left = left + 1
+        right = right - 1
     return True
 
 
-# src_str = "madam"
-src_str = "ABCBa"
-print("The given string is palindrome: ", is_palindrome(src_str))
+def main():
+    test_strings = ["madam", "ABCB", "abcdcba", "abba", "MaDam"]
+    for test in test_strings:
+        print(f"The given string {test} is palindrome: ", is_palindrome(test))
+
+
+if __name__ == "__main__":
+    main()
